364c6957e401b1b18b3a3d33384d6658fdccfd3f
kenji tomita <destinys.lotus.1108mn@gmail.com>
2015-10-12 12:49:18 +0900

    Added animation

--- app/src/main/java/tokyo/tommy_kw/viewanimator/ViewAnimator.kt
+++ app/src/main/java/tokyo/tommy_kw/viewanimator/ViewAnimator.kt
@@ -1,11 +1,14 @@
 package tokyo.tommy_kw.viewanimator
 
+import android.animation.Animator
+import android.animation.AnimatorSet
 import android.animation.ObjectAnimator
 import android.animation.PropertyValuesHolder
 import android.app.Activity
 import android.util.Log
 import android.view.View
 import android.widget.ImageView
+import java.util.*
 
 /**
  * Created by tommy on 15/10/11.
@@ -33,4 +36,25 @@ fun ImageView.anim01(degree:Double, distance:Float, duration: Long = DURATION) {
         val holderY = PropertyValuesHolder.ofFloat("translationX", 0f, toY)
         val rotation = PropertyValuesHolder.ofFloat("rotation", 0f, 360f)
         ObjectAnimator.ofPropertyValuesHolder(this, holderX, holderY, rotation).setDuration(duration).start()
+}
+
+fun ImageView.anim02(degree:Double, distance:Float, duration: Long = DURATION) {
+        val animList = ArrayList<Animator>()
+        val alphaAnim = ObjectAnimator.ofFloat(this, "alpha", 0f, 1f)
+        alphaAnim.setDuration(duration)
+        animList.add(alphaAnim)
+
+        val toX = (distance * Math.cos(Math.toRadians(degree))) as Float
+        val toY = (distance * Math.sin(Math.toRadians(degree))) as Float
+
+        val holderX = PropertyValuesHolder.ofFloat("translationX", 0f, toX)
+        val holderY = PropertyValuesHolder.ofFloat("translationX", 0f, toY)
+        val rotation = PropertyValuesHolder.ofFloat("rotation", 0f, 360f)
+
+        val translationAnim = ObjectAnimator.ofPropertyValuesHolder(this, holderX, holderY, rotation)
+        translationAnim.setDuration(duration)
+        animList.add(translationAnim)
+        val animSet = AnimatorSet()
+        animSet.playSequentially(animList)
+        animSet.start()
 }
