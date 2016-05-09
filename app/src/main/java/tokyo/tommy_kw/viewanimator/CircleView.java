0a6f5bbf0eeb9a08f1cfc9fad7cd50b5a3288669
kenji tomita <destinys.lotus.1108mn@gmail.com>
2016-05-10 08:49:55 +0900

    [WIP] implements CircleView

--- app/src/main/java/tokyo/tommy_kw/viewanimator/CircleView.java
+++ app/src/main/java/tokyo/tommy_kw/viewanimator/CircleView.java
@@ -2,6 +2,9 @@ package tokyo.tommy_kw.viewanimator;
 
 import android.animation.ArgbEvaluator;
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Paint;
 import android.util.AttributeSet;
 import android.view.View;
 
@@ -12,6 +15,19 @@ public class CircleView extends View {
     private static int START_COLOR = 0xFFFF1000;
     private static int END_COLOR = 0xFFFF2000;
     private ArgbEvaluator argbEvaluator = new ArgbEvaluator();
+
+    private Paint circlePaint = new Paint();
+    private Paint maskPaint = new Paint();
+
+    private Bitmap tmpBitmap;
+    private Canvas tmpCanvas;
+
+    private float outerCircleRadiusProgress = 0f;
+    private float innerCircleRadiusProgress = 0f;
+
+    private int width = 0;
+    private int height = 0;
+    private int maxCircleSize;
 
     public CircleView(Context context) {
         super(context);
